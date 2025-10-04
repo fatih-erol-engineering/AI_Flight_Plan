583cda13ef3ba3bc0fb5dd4f5b2f0cfe2ff626c9
Fatih Erol <fatih.erol.engineering@gmail.com>
2025-10-04 14:40:58 +0300

    Add Icons and Change UI Button Style
    Scripts are blowed up

--- AI_Flight_Plan_Unity/Assets/ScriptableObjects/Theme/Theme.cs
+++ AI_Flight_Plan_Unity/Assets/ScriptableObjects/Theme/Theme.cs
@@ -3,10 +3,27 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Theme", menuName = "Scriptable Objects/Theme")]
 public class Theme : ScriptableObject
 {
-    public Texture2D mouseDefault;    
+
+    public Texture2D mouseDefault;
     public Texture2D mouseDrag;
     public GameObject waypointPrefab;
     public GameObject controlPointPrefab;
     public GameObject trajectoryPrefab;
     public GameObject BSplineSegmentPrefab;
+    [Header("Menu Icons")]
+    public Texture2D addIcon;
+    public Texture2D editIcon;
+    public Texture2D fixedWingIcon;
+    public Texture2D rotorIcon;
+    public Texture2D listenIcon;
+    public Texture2D playIcon;
+    public Texture2D stopIcon;
+    public Texture2D pauseIcon;
+
+    [Header("Theme Colors")]
+    public Color color1;
+    public Color color2;
+    public Color color3;
+    public Color color4;
+    public Color color5;
 }
