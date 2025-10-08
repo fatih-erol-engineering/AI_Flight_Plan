175a8d632a3b7dbcac71d2d7089f8a2b9593ada3
Fatih Erol <b.sanlan.1325@gmail.com>
2025-10-08 13:32:15 +0300

    Feat Add Btn Active and Deactive

--- AI_Flight_Plan_Unity/Assets/UI/UIManager.cs
+++ AI_Flight_Plan_Unity/Assets/UI/UIManager.cs
@@ -1,6 +1,9 @@
 
 using UnityEngine.UIElements;
 using UnityEngine;
+using System.Linq;
+
+
 
 
 public class UIManager : MonoBehaviour
@@ -9,28 +12,22 @@ public class UIManager : MonoBehaviour
     public UIDocument uiDocument;
     public Camera cam;
 
-    public VisualElement root, mainMenuRoot, addRoot;
-    public ToggleButtonGroup mainMenuTBG, addTBG;
-    public DropdownField aircraftListDDM;
-    public Button addBtn, editBtn, listenBtn, settingsBtn, rotorBtn, fixedWingBtn;    
-    public Slider timeSlider;
-
+    private VisualElement root, mainMenuRoot, addRoot;
+    private ToggleButtonGroup mainMenuTBG, addTBG;
+    private DropdownField aircraftListDDM;
+    private Button addBtn, editBtn, listenBtn, settingsBtn, rotorBtn, fixedWingBtn;    
+    private Slider timeSlider;
+    
 
     void Start()
     {
-        startData();
-        ResetToDefault();
+        BindData();        
     }
     void Update()
     {
-        ResetToDefault_Check();
-        testCheck();        
+        ResetToDefault_Check();      
     }
-    private void testCheck()
-    {
-        bool idx = mainMenuTBG.value[0];
-        Debug.Log(idx);
-    }
+
 
     void ResetToDefault_Check()
     {
@@ -39,7 +36,7 @@ public class UIManager : MonoBehaviour
             ResetToDefault();
         }
     }
-    void startData()
+    void BindData()
     {
         if (!uiDocument) uiDocument = GetComponent<UIDocument>();
         if (!cam) cam = Camera.main;
@@ -51,24 +48,85 @@ public class UIManager : MonoBehaviour
 
         mainMenuTBG = root.Q<ToggleButtonGroup>("mainTBG");
         addTBG = root.Q<ToggleButtonGroup>("addTBG");
-
-        aircraftListDDM = root.Q<DropdownField>("aircraftListDDM");
-
+        
         addBtn = root.Q<Button>("addBtn");        
         editBtn = root.Q<Button>("editBtn");
         listenBtn = root.Q<Button>("listenBtn");
         settingsBtn = root.Q<Button>("settingsBtn");
         rotorBtn = root.Q<Button>("rotorBtn");
         fixedWingBtn = root.Q<Button>("fixedWingBtn");
+
+        aircraftListDDM = root.Q<DropdownField>("aircraftListDDM");
+
+        addBtn.RegisterCallback<ClickEvent>(OnAddBtnClicked);
+        rotorBtn.RegisterCallback<ClickEvent>(OnRotorBtnClicked);
+        fixedWingBtn.RegisterCallback<ClickEvent>(OnFixedWingBtnClicked);        
+        editBtn.RegisterCallback<ClickEvent>(OnEditBtnClicked);
+        ResetToDefault();
     }
     void ResetToDefault()
     {
         mainMenuRoot.style.display = DisplayStyle.Flex;
-        addRoot.style.display = DisplayStyle.None;
+
+        addRoot.AddToClassList("submenusHidden");
+        aircraftListDDM.AddToClassList("submenusHidden");
+
+        ResetToggleButtonGroups(mainMenuTBG);
+        ResetToggleButtonGroups(addTBG);        
     }
- 
+    void ResetToggleButtonGroups(ToggleButtonGroup tbg) 
+    {
+        var st = tbg.value;
+        st.ResetAllOptions();
+        tbg.value = st;
+    }
 
+    void OnAddBtnClicked(ClickEvent clickEvent)
+    {
+        bool isSelected = mainMenuTBG.value[0];
+        if (isSelected)
+        {
+            addRoot.RemoveFromClassList("submenusHidden");            
+        }
+        else
+        {
+            addRoot.AddToClassList("submenusHidden");            
+        }
+    }
+    void OnRotorBtnClicked(ClickEvent clickEvent)
+    {
+        bool isSelected = addTBG.value[0];
+        if (isSelected)
+        {
+            aircraftListDDM.RemoveFromClassList("submenusHidden");
+        }
+        else
+        {
+            aircraftListDDM.AddToClassList("submenusHidden");
+        }
+    }
+    void OnFixedWingBtnClicked(ClickEvent clickEvent)
+    {
+        bool isSelected = addTBG.value[1];
+        if (isSelected)
+        {
+            aircraftListDDM.RemoveFromClassList("submenusHidden");
+        }
+        else
+        {
+            aircraftListDDM.AddToClassList("submenusHidden");
+        }
+    }
 
-
+    void OnEditBtnClicked(ClickEvent clickEvent)
+    {
+        bool isSelected = mainMenuTBG.value[1];
+        if (isSelected)
+        {
+            addRoot.AddToClassList("submenusHidden");
+            aircraftListDDM.AddToClassList("submenusHidden");
+            ResetToggleButtonGroups(addTBG);
+        }
+    }
 
 }
