da6ab7818c6c28a0352ffc7a182d35c0560dd7c9
Fatih Erol <fatih.erol.engineering@gmail.com>
2025-10-28 22:05:37 +0300

    Start Adding Cesium

---
+++ AI_Flight_Plan_Unity/Assets/Editor/TubeManagerEditorUI.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(TubeManager))]
+[ExecuteAlways]
+public class TubeManagerEditorUI : Editor
+{
+    // local editor field (keeps value while editor session active)
+    private Vector3 leftVector = Vector3.zero;
+
+    public override void OnInspectorGUI()
+    {
+
+
+        DrawDefaultInspector();
+        TubeManager myScript = (TubeManager)target;
+
+        // Horizontal row: Vector3 field on left, action button on right
+        EditorGUILayout.BeginHorizontal();
+        leftVector = EditorGUILayout.Vector3Field(GUIContent.none, leftVector, GUILayout.Width(220));
+        if (GUILayout.Button("Check Pos Inside ", GUILayout.Height(20)))
+        {
+            myScript.CheckPositionInsideOrNot(leftVector);
+        }
+        EditorGUILayout.EndHorizontal();
+
+
+    }
+}
